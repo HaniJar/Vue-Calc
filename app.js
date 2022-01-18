798cc09f1782e6e279c1ad97cad3b17d3de98f29
Haniah Jardien <jardienhaniah@gmail.com>
2022-01-18 13:06:24 +0200

    pHew

---
+++ app.js
@@ -0,0 +1,36 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      sum: "",
+      operators: [
+        "1",
+        "2",
+        "3",
+        "+",
+        "4",
+        "5",
+        "6",
+        "-",
+        "7",
+        "8",
+        "9",
+        "*",
+        "/",
+        ".",
+      ],
+    };
+  },
+  methods: {
+    addToSum(operator) {
+      this.sum += operator;
+    },
+    evalSum() {
+      this.sum = eval(this.sum);
+    },
+    clearSum() {
+      this.sum = "";
+    },
+  },
+});
+
+app.mount("#app");
